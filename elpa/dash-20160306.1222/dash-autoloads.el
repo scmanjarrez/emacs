c4d654456207bbc06f05d116addc5dc9f83107d2
scmanjarrez <s.cmanjarrez@alumnos.upm.es>
2016-03-20 23:42:27 +0100

    emacs base config

---
+++ elpa/dash-20160306.1222/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22252 37083 324428 679000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
