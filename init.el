06a113c6fd372298a133dd8672c4a9c591571883
scmanjarrez <s.cmanjarrez@alumnos.upm.es>
2017-02-26 20:10:39 +0100

    Added web-mode package

--- init.el
+++ init.el
@@ -307,3 +307,13 @@ Also returns nil if pid is nil."
 (global-set-key [f9] 'highlight-or-dehighlight-line)
 
 (xclip-mode 1)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
