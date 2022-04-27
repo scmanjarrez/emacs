0964868f7d186a9ae4e85419e54318770ec84781
scmanjarrez <serchiman@gmail.com>
2022-04-27 16:44:53 +0200

    Updated latex command color

--- init.el
+++ init.el
@@ -70,6 +70,7 @@
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
+    '(font-latex-sedate-face ((t (:foreground "#adc8ff"))))
    )
 
   ;; Loads emacs fullscreened
