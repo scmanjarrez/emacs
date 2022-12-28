6c95cc92ca5aaf349baf8ad478d2c1a746005516
scmanjarrez <serchiman@gmail.com>
2022-12-28 23:08:40 +0100

    Added tramp to gitignore. Remove old helm frame center function

--- init.el
+++ init.el
@@ -429,50 +429,6 @@
       (overlay-put overlay-highlight 'line-highlight-overlay-marker t))))
 (global-set-key [f9] 'my-toggle-highlight-line)
 
-(defun my-helm-display-frame-center (buffer &optional resume)
-  "Display `helm-buffer' in a separate frame which centered in
-parent frame."
-  (if (not (display-graphic-p))
-      ;; Fallback to default when frames are not usable.
-      (helm-default-display-buffer buffer)
-    (setq helm--buffer-in-new-frame-p t)
-    (let* ((parent (selected-frame))
-           (frame-pos (frame-position parent))
-           (parent-left (car frame-pos))
-           (parent-top (cdr frame-pos))
-           (width (/ (frame-width parent) 2))
-           (height (/ (frame-height parent) 3))
-           tab-bar-mode
-           (default-frame-alist
-             (if resume
-                 (buffer-local-value 'helm--last-frame-parameters
-                                     (get-buffer buffer))
-               `((parent . ,parent)
-                 (width . ,width)
-                 (height . ,height)
-                 (undecorated . ,helm-use-undecorated-frame-option)
-                 (left-fringe . 0)
-                 (right-fringe . 0)
-                 (tool-bar-lines . 0)
-                 (line-spacing . 0)
-                 (desktop-dont-save . t)
-                 (no-special-glyphs . t)
-                 (inhibit-double-buffering . t)
-                 (tool-bar-lines . 0)
-                 (left . ,(+ parent-left (/ (* (frame-char-width parent) (frame-width parent)) 4)))
-                 (top . ,(+ parent-top (/ (* (frame-char-width parent) (frame-height parent)) 6)))
-                 (title . "Helm")
-                 (vertical-scroll-bars . nil)
-                 (menu-bar-lines . 0)
-                 (fullscreen . nil)
-                 (visible . ,(null helm-display-buffer-reuse-frame))
-                 ;; (internal-border-width . ,(if IS-MAC 1 0))
-                )))
-           display-buffer-alist)
-      (set-face-background 'internal-border (face-foreground 'default))
-      (helm-display-buffer-popup-frame buffer default-frame-alist))
-    (helm-log-run-hook 'helm-window-configuration-hook)))
-
 ;; Custom function to display helm mini-frame in the center of the screen
 ;; original code: https://github.com/emacs-helm/helm/blob/master/helm-core.el
 ;; original modification: https://www.reddit.com/r/emacs/comments/jj269n/display_helm_frames_in_the_center_of_emacs/
